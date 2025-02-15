423ac6cbf82eb4483a69f2ee6c0588845c4401ac
trivien11vn <triviencoderr@gmail.com>
2025-02-15 16:58:51 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use \App\Http\Controllers\Admin\Users\LoginController;
+use \App\Http\Controllers\Admin\MainController;
+
+
+Route::get('admin/users/login', [LoginController::class, 'index']);
+Route::post('admin/users/login/store', [LoginController::class, 'store']);
+
+Route::get('admin/main', [MainController::class, 'index']) -> name('admin');
+
+
